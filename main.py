d28f94b551fb391902a5ad407ddd2583388a91a6
alanrabelo <alanrabelo@icloud.com>
2019-04-30 08:29:07 -0300

    Ajustando import de filtros

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ from scipy import misc
 from skimage import data
 from skimage.io import imread
 from skimage.color import rgb2gray
-from skimage import filters
+from skimage.filters import *
 
+ 
 # FUNÇÕES MORFOLOGICAS
 from skimage import morphology
-from skimage.morphology import square, rectangle, disk
+from skimage.morphology import disk
+
 disease_name = 'patient.jpg'
-folha_color = imread(file_name)
+folha_color = imread(disease_name)
 disease = rgb2gray(imread('patient.jpg'))
 
 # Exibe imagens
@@ -25,14 +27,13 @@ plt.axis('off')
 
 open_3  = morphology.opening(disease, disk(3))
 open_15 = morphology.opening(disease, disk(15))
-open_30 = morphology.opening(disease, disk(30))
 difference = open_15 - disease
 global_thresh = threshold_otsu(difference)
 difference = difference > global_thresh
 
 
 # Exibe imagens
-fig = plt.figure(figsize=(100,100))
+fig = plt.figure(figsize=(10,10))
 a = fig.add_subplot(1,4,1)
 plt.imshow(disease, cmap=plt.cm.gray)
 a.set_title('Original')
@@ -48,9 +49,9 @@ plt.imshow(open_15, cmap=plt.cm.gray)
 a.set_title('Abert (15)')
 plt.axis('off')
 
-a = fig.add_subplot(1,4,3)
+a = fig.add_subplot(1,4,4)
 plt.imshow(difference, cmap=plt.cm.gray)
-a.set_title('Abert (15)')
+a.set_title('Diferença')
 plt.axis('off')
 
 plt.tight_layout()
