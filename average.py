4b5b9a7cbd4e27cff91a22de9f2e6d0f11002ed6
alan <alanrabelo@icloud.com>
2019-04-30 15:21:50 -0300

    PCA Analysis

--- average.py
+++ average.py
@@ -13,7 +13,7 @@ from scipy.ndimage.morphology import binary_fill_holes
 # FUNÇÕES MORFOLOGICAS
 from skimage import morphology
 from skimage.morphology import disk
-
+import pickle
 from PIL import Image
 import glob
 
@@ -24,74 +24,130 @@ folders = ['Dataset/Healthy/HealthySpiral',
            'Dataset/Patient/PatientMeander'
            ]
 
-results = {'spiral' : {}, 'meander' : {}}
+healthy_filename = 'healthy.dict'
+disease_filename = 'patient.dict'
 
-sum_of_points = [0, 0]
-valores_healthy = []
-valores_patient = []
-acertos = [0, 0]
-erros = [0, 0]
+try:
+    healthy = pickle.load(open(healthy_filename, 'rb'))
+    patient = pickle.load(open(disease_filename, 'rb'))
+except:
 
-threshold = 51000
+    patient = {}
+    healthy = {}
 
-for index,folder in enumerate(folders):
+    sum_of_points = [0, 0]
+    valores_healthy = []
+    valores_patient = []
+    acertos = [0, 0]
+    erros = [0, 0]
 
-    filenames = glob.glob(folder + '/*.jpg')
-    for filename in filenames:
 
-        patient_name = filename.split('/')[-1].split('-')[-1].split('.')[0]
+    threshold = 51000
+
+    for index,folder in enumerate(folders):
 
 
-        disease = rgb2gray(imread(filename))
+        filenames = glob.glob(folder + '/*.jpg')
+        for filename in filenames:
 
-        open_15 = morphology.opening(disease, disk(15))
-        difference = open_15 - disease
-        global_thresh = threshold_otsu(difference)
-        difference = difference > global_thresh
+            patient_name = filename.split('/')[-1].split('-')[-1].split('.')[0]
 
-        # bw_disease_thresh = threshold_otsu(disease)
-        # bw_disease_image = disease < bw_disease_thresh
-        #
-        # disease_hole_filled = binary_fill_holes(bw_disease_image)
 
-        number_of_points_disease = sum(sum(difference))
+            disease = rgb2gray(imread(filename))
 
-        if index == 0:
 
-            valores_healthy.append(number_of_points_disease)
-            if number_of_points_disease < threshold:
-                acertos[index] += 1
+
+            open_15 = morphology.opening(disease, disk(15))
+            difference = open_15 - disease
+            global_thresh = threshold_otsu(difference)
+            difference = difference > global_thresh
+
+            # bw_disease_thresh = threshold_otsu(disease)
+            # bw_disease_image = disease < bw_disease_thresh
+            #
+            # disease_hole_filled = binary_fill_holes(bw_disease_image)
+
+            number_of_points_disease = sum(sum(difference))
+
+            if index >= 2:
+                if patient_name in patient:
+                    patient[patient_name].append(number_of_points_disease)
+                else:
+                    patient[patient_name] = [number_of_points_disease]
             else:
-                erros[index] += 1
+                if patient_name in healthy:
+                    healthy[patient_name].append(number_of_points_disease)
+                else:
+                    healthy[patient_name] = [number_of_points_disease]
 
 
-        if index == 1:
+    with open(healthy_filename, 'wb') as f:
+        pickle.dump(healthy, f)
 
-            valores_patient.append(number_of_points_disease)
+    with open(disease_filename, 'wb') as f:
+        pickle.dump(patient, f)
 
-            if number_of_points_disease > threshold:
-                acertos[index] += 1
-            else:
-                erros[index] += 1
-
-
-        sum_of_points[index] += number_of_points_disease
-
-    sum_of_points[index] /= len(filenames)
-
-
-# Plotando as somas dos pontos
 
 from matplotlib import pyplot as plt
 
-valores_patient = np.array(valores_patient)
 
-valores_patient = valores_patient[valores_patient>=370000]
+dataset = []
 
-valores_healthy = np.array(valores_healthy)
+for value in healthy.values():
+    new_value = value
+    new_value.append(0)
+    dataset.append(new_value)
 
-valores_healthy = valores_healthy[valores_healthy>=370000]
+for value in patient.values():
+    new_value = value
+    new_value.append(1)
+    dataset.append(new_value)
 
-plt.plot(valores_patient, 'o')
-plt.plot(valores_healthy, 'o')
+
+
+from sklearn.preprocessing import MinMaxScaler
+import itertools
+
+dataset = MinMaxScaler().fit_transform(dataset)
+
+combinations = list(itertools.combinations([0, 1, 2, 3, 4, 5, 6, 7], 2))
+
+dataset = np.array(dataset)
+
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2)
+principalComponents = pca.fit_transform(dataset)
+print(pca.explained_variance_ratio_)
+new_array = []
+
+for index,data in enumerate(dataset):
+    new_data = list(principalComponents[index, :])
+    new_data.append(data[-1])
+    new_array.append(new_data)
+
+for data in new_array:
+
+    y = int(data[-1])
+    if y == 0:
+        plt.plot(data[0], data[1], 'bo')
+    else:
+        plt.plot(data[0], data[1], 'ro')
+
 plt.show()
+
+
+
+# for combination in combinations:
+#
+#     for value in dataset:
+#
+#         y = int(value[-1])
+#         x1 = value[combination[0]]
+#         x2 = value[combination[1]]
+#
+#         if y == 0:
+#             plt.plot(x1, x2, 'bo')
+#         else:
+#             plt.plot(x1, x2, 'ro')
+#
+#     plt.show()
